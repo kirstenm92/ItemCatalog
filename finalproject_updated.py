d6b27ca3a3c4d29de75661306214513feff1a91f
kirstenm92 <kirstenm92@gmail.com>
2017-03-15 13:42:54 +0200

    Update finalproject_updated.py

--- finalproject_updated.py
+++ finalproject_updated.py
@@ -122,11 +122,11 @@ def gconnect():
     login_session['provider'] = 'google'
 
     # See if user exists, if it doesn't, make a new one
-    if not getUserID(login_session['email']):
-        user_id = createUser(login_session)
-        login_session['user_id'] = user_id
-        #newUser = User(name = 'username', email = 'email', picture = 'picture')
-
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+    	user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+	
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
